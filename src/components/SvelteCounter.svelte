8a911779266ad48b7e3eea5846b8313df916c572
Mohammed Alshahrani <mmogib@gmail.com>
2021-12-31 08:26:28 +0300

    init: initial one

---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+</script>
+
+<script lang="ts">
+  let counter: number = 0;
+
+  const addOne = (count: number): number => {
+    return count + 1;
+  };
+
+  const subtract = (count: number): number => {
+    return count - 1;
+  };
+</script>
+
+<div id="svelte" class="counter">
+  <button on:click={() => (counter = subtract(counter))}>-</button>
+  <pre>{counter}</pre>
+  <button on:click={() => (counter = addOne(counter))}>+</button>
+</div>
